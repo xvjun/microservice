54376a5689827deba1ff8d66abb86e831bf6831f
Xu Jun <xujun@4paradigm.com>
2020-01-20 15:08:40 +0800

    update 2

--- course-edge-service/build.sh
+++ course-edge-service/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn clean package
-docker build -t 172.27.160.34:8090/micro-service/course-edge-service:latest .
-docker push 172.27.160.34:8090/micro-service/course-edge-service:latest
+docker build -t 172.27.160.10:8090/micro-service/course-edge-service:latest .
+docker push 172.27.160.10:8090/micro-service/course-edge-service:latest
